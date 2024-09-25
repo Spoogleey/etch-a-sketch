7da024ba8af1c8add3fe3811dcdacc940389d1ca
Liam Green <122028106+Spoogleey@users.noreply.github.com>
2024-09-25 15:44:05 +0100

    Tidied up the javascript for changing the grid
    Tidied up changing the grid size by making it into a function. The function is called when the "Change the grid size" button is clicked, or when the webpage is first loaded.

--- script.js
+++ script.js
@@ -1,33 +1,20 @@
 const gridContainer = document.querySelector("#grid-container");
 const gridSizeButton = document.querySelector(".grid-size");
 const gridSizeDisplay = document.querySelector(".current-size")
-let size = 0;
 
-size = Number(prompt("Please state how big you would like the grid to be.\nFor example, if you enter 20, then the grid will be 20x20 (Maximum size of 100):"));
-gridSizeDisplay.textContent = `Current grid size = ${size} x ${size}`;
+window.onload = () => changeGridSize();
+gridSizeButton.onclick = () => changeGridSize();
 
-for(let i = 0; i < size; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    
-    for(let j = 0; j < size; j++) {
-        const column = document.createElement("div");
-        column.style.cssText = "border: 1px solid black;";
-        column.classList.add("column");
-        row.appendChild(column);
-    }
-    gridContainer.appendChild(row);
-}
-
-gridSizeButton.addEventListener("click", () => {
+function changeGridSize() {
     const rows = document.querySelectorAll(".row");
     rows.forEach((row) => {
         row.remove();
     });
 
-    size = Number(prompt("Please state how big you would like the grid to be.\nFor example, if you enter 20, then the grid will be 20x20 (Maximum size of 100):"));
+    let size = Number(prompt("Please state how big you would like the grid to be.\nFor example, if you enter 20, then the grid will be 20x20 (Maximum size of 100):"));
 
-    if(size == 0) {
+    if(size == 0 || size > 100) {
+        alert("Please enter a valid size.");
         size = 16;
     }
 
@@ -53,12 +40,4 @@ gridSizeButton.addEventListener("click", () => {
         columns.style.backgroundColor = "black";
     });
 });
-});
-
-const gridColumn = document.querySelectorAll(".column");
-
-gridColumn.forEach((columns) => {
-    columns.addEventListener("mouseover", () => {
-        columns.style.backgroundColor = "black";
-    });
-});
+}
