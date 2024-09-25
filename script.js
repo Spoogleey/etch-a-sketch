54066cfc7e6260ed933a52b8050f518f1e8e4226
Liam Green <122028106+Spoogleey@users.noreply.github.com>
2024-09-25 14:47:57 +0100

    Added the buttons
    Added the buttons at the top of the screen for changing the size of the grid and also resetting the grid. In between the two buttons is a div that will display the current size of the grid.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const gridContainer = document.querySelector("#grid-container");
 
-for(let i = 0; i < 50; i++) {
+for(let i = 0; i < 16; i++) {
     const row = document.createElement("div");
     row.classList.add("row");
     
-    for(let j = 0; j < 50; j++) {
+    for(let j = 0; j < 16; j++) {
         const column = document.createElement("div");
         column.style.cssText = "border: 1px solid black;";
         column.classList.add("column");
