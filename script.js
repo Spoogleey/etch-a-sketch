dbee0ece3dfc60222e4db0c9810d38aeebe1cef9
Liam Green <122028106+Spoogleey@users.noreply.github.com>
2024-09-25 15:55:06 +0100

    Added comments
    Added comments to explain some of the functions.

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+// Create constants of the main container and the buttons
 const gridContainer = document.querySelector("#grid-container");
 const gridSizeButton = document.querySelector(".grid-size");
 const gridSizeDisplay = document.querySelector(".current-size")
 const resetGridButton = document.querySelector(".reset");
 
+// Call the changeGridSize() function when the page loads
 window.onload = () => changeGridSize();
 gridSizeButton.onclick = () => changeGridSize();
 resetGridButton.onclick = () => resetGrid();
 
+// Function for changing the grid size via user input
 function changeGridSize() {
     const rows = document.querySelectorAll(".row");
     rows.forEach((row) => {
@@ -15,11 +18,13 @@ function changeGridSize() {
 
     let size = Number(prompt("Please state how big you would like the grid to be.\nFor example, if you enter 20, then the grid will be 20x20 (Maximum size of 100):"));
 
+    // Check if the user inputted an invalid number
     if(size == 0 || size > 100) {
         alert("Please enter a valid size.");
         size = 16;
     }
 
+    // Create the rows and columns based on the user input
     for(let i = 0; i < size; i++) {
         const row = document.createElement("div");
         row.classList.add("row");
@@ -35,6 +40,7 @@ function changeGridSize() {
 
     gridSizeDisplay.textContent = `Current grid size = ${size} x ${size}`;
 
+    // Change the background colour of the div when the user hovers over it
     const gridColumn = document.querySelectorAll(".column");
 
     gridColumn.forEach((columns) => {
@@ -44,10 +50,11 @@ function changeGridSize() {
 });
 }
 
+// Function for resetting the grid (removing the background colour)
 function resetGrid() {
     const gridColumn = document.querySelectorAll(".column");
 
     gridColumn.forEach((columns) => {
-        columns.style.backgroundColor = "white";
+        columns.style.removeProperty("background-color");
     });
 }
