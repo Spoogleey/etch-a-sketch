6889abfc1ed7e48c839a8b294a8e720512a4e515
Liam Green <122028106+Spoogleey@users.noreply.github.com>
2024-09-25 11:20:37 +0100

    Created the grid
    Added the code that creates a 16 x 16 grid and adds it to the container div.

---
+++ script.js
@@ -0,0 +1,14 @@
+const gridContainer = document.querySelector(".grid-container");
+
+for(let i = 0; i < (16); i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    gridContainer.appendChild(row);
+    
+    for(let j = 0; j < 16; j++) {
+        const column = document.createElement("div");
+        column.style.cssText = "border: 1px solid black;";
+        column.classList.add("column");
+        row.appendChild(column);
+    }
+}
