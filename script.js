c41f4a52085d54ec1357520b5134dce5278bf421
Liam Green <122028106+Spoogleey@users.noreply.github.com>
2024-09-25 15:18:39 +0100

    Change grid size button working
    Clicking on the "Change grid size button" and entering a number below 100 will delete the original grid and replace it with a grid that is equal to the size that you entered.

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const gridContainer = document.querySelector("#grid-container");
+const gridSizeButton = document.querySelector(".grid-size");
+const gridSizeDisplay = document.querySelector(".current-size")
+let size = 0;
 
-for(let i = 0; i < 16; i++) {
+size = Number(prompt("Please state how big you would like the grid to be.\nFor example, if you enter 20, then the grid will be 20x20 (Maximum size of 100):"));
+gridSizeDisplay.textContent = `Current grid size = ${size} x ${size}`;
+
+for(let i = 0; i < size; i++) {
     const row = document.createElement("div");
     row.classList.add("row");
     
-    for(let j = 0; j < 16; j++) {
+    for(let j = 0; j < size; j++) {
         const column = document.createElement("div");
         column.style.cssText = "border: 1px solid black;";
         column.classList.add("column");
@@ -12,6 +18,42 @@ for(let i = 0; i < 16; i++) {
     }
     gridContainer.appendChild(row);
 }
+
+gridSizeButton.addEventListener("click", () => {
+    const rows = document.querySelectorAll(".row");
+    rows.forEach((row) => {
+        row.remove();
+    });
+
+    size = Number(prompt("Please state how big you would like the grid to be.\nFor example, if you enter 20, then the grid will be 20x20 (Maximum size of 100):"));
+
+    if(size == 0) {
+        size = 16;
+    }
+
+    for(let i = 0; i < size; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        
+        for(let j = 0; j < size; j++) {
+            const column = document.createElement("div");
+            column.style.cssText = "border: 1px solid black;";
+            column.classList.add("column");
+            row.appendChild(column);
+        }
+        gridContainer.appendChild(row);
+    }
+
+    gridSizeDisplay.textContent = `Current grid size = ${size} x ${size}`;
+
+    const gridColumn = document.querySelectorAll(".column");
+
+    gridColumn.forEach((columns) => {
+    columns.addEventListener("mouseover", () => {
+        columns.style.backgroundColor = "black";
+    });
+});
+});
 
 const gridColumn = document.querySelectorAll(".column");
 
